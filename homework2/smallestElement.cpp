90fa7d78acd4e362934c05c24973332a3cebd50d
Derek21298 <derek21298@gmail.com>
2018-09-27 22:31:20 -0400

    added output screenshot

--- homework2/smallestElement.cpp
+++ homework2/smallestElement.cpp
@@ -5,30 +5,38 @@
 //	Description: Using pointers to find smallest element in an array of integers
 //
 
-#incldue <iostream>
+#include <iostream>
 
 using namespace std;
 
-int smallestElement(int list, int size);
+int smallestElement(int list[], int size);
 
-int main(){
+int main(void){
 	
+	// Array of values to be tested from homework handout
 	int testArray[] = {1, 2, 4, 5, 10, 100, 2, -22};
-
-	size = testArrary.size();
-
-	smallestValue = smallestElement(testArray, size);
-
+	
+	// Getting the size of array
+	int size = sizeof(testArray) / sizeof(testArray[0]);
+	
+	// Get the smallest value in the array
+	int smallestValue = smallestElement(testArray, size);
+	
+	// Print out to console
 	cout << "The smallest element in the array is:" << smallestValue << endl;
+		
+	return 0;
 }
 
-int smallestElement(int list, int size){
+int smallestElement(int list[], int size){
 	
+	// Set minimum value to first value
 	int min = *list;
 	int i;
-
+	
 	for(i = 0; i < size; i++){
-
+		
+		// If value is lower than min, make it the new min
 		if(*(list + i) < min){
 			min = *(list + i);
 		}
