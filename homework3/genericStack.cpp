69f0dea9101075f4be9ac2a20e16417d1daab693
Derek21298 <derek21298@gmail.com>
2018-10-03 13:26:32 -0400

    Commented Homework4

--- homework3/genericStack.cpp
+++ homework3/genericStack.cpp
@@ -13,19 +13,22 @@
 using namespace std;
 
 int main(int argc, char **argv){
-
+	
+	// Create stack object with type string
 	Stack<string> inputs;
 	
+	// Push Bird, Cat, and Dog to the stack
 	cout << "Pushing these strings to Stack: Bird, Cat, Dog" << endl << endl;
 	inputs.push("Bird");
 	inputs.push("Cat");
 	inputs.push("Dog");	
 	
+	// Check if the stack is empty (Obviously not as stack just got pushed)
 	cout << "Is the stack empty: ";
 	if (inputs.empty() == 1) cout << "True" << endl << endl;
 	else cout << "False" << endl << endl;
 	
-
+	// Loop 3 times to check size, peek at stack, and pop element
 	for (int i = 0; i < 3; i++) {	
 
 	cout << "Stack size: " << inputs.getSize() << endl;
@@ -34,9 +37,10 @@ int main(int argc, char **argv){
 	
 	}
 
+	// Check if the stack is empty (Obviously yes as all 3 elements are popped)
 	cout << "Is the stack empty: ";
 	if (inputs.empty() == 1) cout << "True" << endl;
 	else cout << "False" << endl;
-		
+
 }
 
--- homework3/genericStack.h
+++ homework3/genericStack.h
@@ -7,12 +7,13 @@
 
 #include <vector>
 
-
 using namespace std;
 
+// Create the stack class to be used
 template<typename T>
 class Stack {
 
+// Create functions of the stack to be used
 public: 
 	Stack();
 	bool empty() const;
@@ -21,32 +22,36 @@ public:
 	T pop();
 	int getSize() const;
 
+// Keep elements in the stack private
 private:
 	vector<T> elements;
 	int size;
 };
 
-
-
+// Create constructor
 template<typename T> 
 Stack<T>::Stack() {
 }
 
+// Create empty() that tells in the stack is empty or not (1 True: 0 False)
 template<typename T>
 bool Stack<T>::empty() const {
 	return (elements.size() == 0);
 }
 
+// Create peek() to show top element of the stack
 template<typename T>
 T Stack<T>::peek() const {
 	return elements.at(elements.size() - 1);
 }
 
+// Create push() to push the element to the top of the stack
 template<typename T>
 void Stack<T>::push(T number){
 	elements.push_back(number);
 }
 
+// Create pop() to pop the top element off the stack
 template<typename T>
 T Stack<T>::pop() {
 	T element = elements.at(elements.size() - 1);
@@ -54,6 +59,7 @@ T Stack<T>::pop() {
 	return element;
 }
 
+// Create getSize() to return the size of the stack
 template<typename T>
 int Stack<T>::getSize() const {
 	return elements.size();
