bd9871fa431002bbe3a0ee6f6e847c0eae23cd81
Derek21298 <derek21298@gmail.com>
2018-10-24 20:06:42 -0400

    Added homework 4 code

---
+++ homework4/myPoint.cpp
@@ -0,0 +1,44 @@
+//
+//	COS 221 Homework #4
+//	Author: Derek Haas
+//	Date: 10/24/18
+//	Description: Creates two points (x, y, z) and (x1, y2, z3) and calculates
+//		     distance between them
+//
+
+#include <iostream>
+#include "myPoint.h"
+#include <vector>
+
+using namespace std;
+
+
+int main(void){
+
+	// Create point1 at (0,0,0)
+	ThreeDPoint point1;
+	
+	// Create point2 at (10,30,25.5)
+	ThreeDPoint point2(10, 30, 25.5);
+
+	// Get the x, y, and z coordinates of both points
+	double point1_xLoc = point1.getX();
+	double point1_yLoc = point1.getY();
+	double point1_zLoc = point1.getZ();
+
+	double point2_xLoc = point2.getX();
+	double point2_yLoc = point2.getY();
+	double point2_zLoc = point2.getZ();
+
+	
+	// Print out the coordinates to the consol
+	cout << "First Point: (" << point1_xLoc << ", " << point1_yLoc << ", " << point1_zLoc << ")" << endl;
+	cout << "Second Point: (" << point2_xLoc << ", " << point2_yLoc << ", " << point2_zLoc << ")" << endl;
+
+	// Calculate the distance and print
+	cout << "Distance between these points is: " << point1.distance(point2) << endl;
+
+	return 0;	
+
+
+}
---
+++ homework4/myPoint.h
@@ -0,0 +1,99 @@
+//
+//	COS 221 Homework #4
+//	Author: Derek Haas
+//	Date: 10/24/18
+//	Description: Creates two points (x, y, z) and (x1, y2, z3) and calculates
+//		     distance between them
+//
+
+#include <iostream>
+#include <cmath>
+
+
+using namespace std;
+
+
+// Create a class that makes a 2D point (x,y)
+class MyPoint {
+
+	protected:
+
+		double x;
+		double y;
+
+	public:
+
+		MyPoint();			
+		MyPoint(double, double);
+		double getX();
+		double getY();
+		double distance(MyPoint);
+
+};
+
+// No arg constructor to make point at (0,0)
+MyPoint::MyPoint() {
+	x = 0;
+	y = 0;
+}
+
+// Constructor to make point at (xLoc, yLoc)
+MyPoint::MyPoint(double xLoc, double yLoc) {
+	x = xLoc;
+	y = yLoc;
+}
+
+// Return the value of the x coordinate
+double MyPoint::getX() {
+	return x;
+}
+
+// Return the value of the y coordinate
+double MyPoint::getY() {
+	return y;
+}
+
+// Calculate the distance between the two points and return it
+// sqrt( (x1-x2)^2 + (y1-y2)^2 ) is the distance between points
+double MyPoint::distance(MyPoint thePoint) {
+	double distance = sqrt(pow(thePoint.y - y, 2) + pow(thePoint.x - x, 2));
+	return distance;
+}
+
+// Create derived class "ThreeDPoint" of "MyPoint"
+class ThreeDPoint: public MyPoint {
+
+	private: 
+		
+		double z;
+		
+	public:
+		ThreeDPoint();
+		ThreeDPoint(double, double, double);
+		double getZ();
+		const double distance(const ThreeDPoint&);
+};
+
+// No arg constructor to set the z coordinate as 0
+// MyPoint no arg constructor is also called to initialize x and y  
+ThreeDPoint::ThreeDPoint() {
+	z = 0;
+}
+
+// Constructor to set the z coordinate as zLoc
+// MyPoint constructor is also called to initialize x and y
+ThreeDPoint::ThreeDPoint(double xLoc, double yLoc, double zLoc) : MyPoint(xLoc, yLoc) {
+	z = zLoc;
+}
+
+// Return the value of the z coordinate
+double ThreeDPoint::getZ() {
+	return z;
+}
+
+// Calculate the distance between two 3D points and return it
+// sqrt( (x1-x2)^2 + (y1-y2)^2 + (z1-z2)^2 ) is the distance between the points
+const double ThreeDPoint::distance(const ThreeDPoint& theCoords) {
+	const double distance = sqrt(pow(theCoords.z - z, 2) + pow(theCoords.y - y, 2) + pow(theCoords.x - x, 2));
+	return distance;
+}
